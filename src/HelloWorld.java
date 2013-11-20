f8c1e9c98460c7ec67cb4f426fdc46536bab1c30
PeterVasilevsky <zeelony@me.com>
2013-11-20 18:21:53 +0400

    Добавлен хелловорлд

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Zeelony
+ * Date: 20.11.13
+ * Time: 18:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
